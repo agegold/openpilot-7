599efdc2800d16f994ff5449c0cd7297924fdeea
Chanho Jung <jungch@naver.com>
2021-11-30 23:55:28 +0900

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -686,7 +686,7 @@ static void ui_draw_turn_signal(UIState *s) { // Hoya modified with Neokii code
     const int h = 140;
     const int gap = -10;
     const int base_y = bdr_s + 10;
-    const int draw_count = 32;
+    const int draw_count = 35;
     int x = center_x;
     int y = base_y + 550;
 
@@ -721,7 +721,7 @@ static void ui_draw_turn_signal(UIState *s) { // Hoya modified with Neokii code
       }
       if(blink_index >= draw_count) {
         blink_index = draw_count - 1;
-        blink_wait = UI_FREQ/5;
+        blink_wait = UI_FREQ/10;
       }
     }
     else {
