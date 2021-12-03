12128f6a2f712cda128a2ed4b5d2bb55b9c64f36
Chanho Jung <jungch@naver.com>
2021-12-03 09:48:14 +0900

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -685,7 +685,7 @@ static void ui_draw_turn_signal(UIState *s) { // Hoya modified with Neokii code
     const int center_x = (s->fb_w - (bdr_s * 2)) / 2 + bdr_s;
     const int w = fb_w / 12;
     const int h = 140;
-    const int gap = -22;
+    const int gap = -27;
     const int base_y = bdr_s + 10;
     const int draw_count = 24;
     int x = center_x;
