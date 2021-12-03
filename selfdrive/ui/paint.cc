990a97b663313479a932d9be6d8b7ffc1e9086c2
Chanho Jung <jungch@naver.com>
2021-12-03 10:07:56 +0900

    IMG

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -683,9 +683,9 @@ static void ui_draw_turn_signal(UIState *s) { // Hoya modified with Neokii code
     const float img_alpha = 1.0f;
     const int fb_w = s->fb_w / 2 - 200;
     const int center_x = (s->fb_w - (bdr_s * 2)) / 2 + bdr_s;
-    const int w = fb_w / 8;
+    const int w = 100;
     const int h = 140;
-    const int gap = -60;
+    const int gap = -80;
     const int base_y = bdr_s + 10;
     const int draw_count = 12;
     int x = center_x;
