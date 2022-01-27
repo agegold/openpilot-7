5a77b8f5e2e79c8846ad5223c2c5b2896686ad71
Chanho Jung <jungch@naver.com>
2022-01-27 23:39:46 +0900

    Update lateral_planner.py

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -143,7 +143,7 @@ class LateralPlanner:
       heading_cost = interp(v_ego, [5.0, 10.0], [MPC_COST_LAT.HEADING, 0.0])
       self.lat_mpc.set_weights(path_cost, heading_cost, self.steer_rate_cost)
       self.laneless_mode_status = True
-    elif self.laneless_mode == 2 and ((self.LP.lll_prob + self.LP.rll_prob)/2 < 0.3) and self.DH.lane_change_state == self.DH.LaneChangeState.off:
+    elif self.laneless_mode == 2 and ((self.LP.lll_prob + self.LP.rll_prob)/2 < 0.3) and self.DH.lane_change_state == log.LateralPlan.laneChangeState.off:
       d_path_xyz = self.path_xyz
       path_cost = np.clip(abs(self.path_xyz[0, 1] / self.path_xyz_stds[0, 1]), 0.5, 1.5) * MPC_COST_LAT.PATH
       # Heading cost is useful at low speed, otherwise end of plan can be off-heading
@@ -152,12 +152,12 @@ class LateralPlanner:
       self.laneless_mode_status = True
       self.laneless_mode_status_buffer = True
     elif self.laneless_mode == 2 and ((self.LP.lll_prob + self.LP.rll_prob)/2 > 0.5) and \
-     self.laneless_mode_status_buffer and self.DH.lane_change_state == self.DH.LaneChangeState.off:
+                                                  self.laneless_mode_status_buffer and self.DH.lane_change_state == log.LateralPlan.laneChangeState.off:
       d_path_xyz = self.LP.get_d_path(v_ego, self.t_idxs, self.path_xyz)
       self.lat_mpc.set_weights(MPC_COST_LAT.PATH, MPC_COST_LAT.HEADING, self.steer_rate_cost)
       self.laneless_mode_status = False
       self.laneless_mode_status_buffer = False
-    elif self.laneless_mode == 2 and self.laneless_mode_status_buffer == True and self.DH.lane_change_state == self.DH.LaneChangeState.off:
+    elif self.laneless_mode == 2 and self.laneless_mode_status_buffer == True and self.DH.lane_change_state == log.LateralPlan.laneChangeState.off:
       d_path_xyz = self.path_xyz
       path_cost = np.clip(abs(self.path_xyz[0, 1] / self.path_xyz_stds[0, 1]), 0.5, 1.5) * MPC_COST_LAT.PATH
       # Heading cost is useful at low speed, otherwise end of plan can be off-heading
