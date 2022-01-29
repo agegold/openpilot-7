6c67137ed22db139651244a7e515854b17dc2ce2
Chanho Jung <jungch@naver.com>
2022-01-29 21:53:36 +0900

    Update desire_helper.py

--- selfdrive/controls/lib/desire_helper.py
+++ selfdrive/controls/lib/desire_helper.py
@@ -69,7 +69,7 @@ class DesireHelper:
     elif carstate.rightBlinker:
       self.lane_change_direction = LaneChangeDirection.right
 
-    if (not active) or (self.lane_change_timer > LANE_CHANGE_TIME_MAX) or (abs(self.output_scale) >= (CP.steerMaxV[0]-0.15) and self.lane_change_timer > 1):
+    if (not active) or (self.lane_change_timer > LANE_CHANGE_TIME_MAX) or (self.lane_change_timer > 1):
       self.lane_change_state = LaneChangeState.off
       self.lane_change_direction = LaneChangeDirection.none
     else:
