da5653ce016a438562ac3f0de24c885e3777bec3
Chanho Jung <jungch@naver.com>
2022-03-22 08:58:37 +0900

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1580,7 +1580,7 @@ static void ui_draw_blindspot_mon(UIState *s) {
 
 // draw date/time/streetname
 void draw_top_text(UIState *s) {
-  int rect_w = 960;
+  int rect_w = 600;
   int rect_x = s->fb_w/2 - rect_w/2;
   const int rect_y = 0;
   const int rect_h = 65;
@@ -1630,23 +1630,23 @@ void draw_top_text(UIState *s) {
     snprintf(now,sizeof(now),"%02d-%02d %s %02d:%02d:%02d ", tm.tm_mon + 1, tm.tm_mday, dayofweek, tm.tm_hour, tm.tm_min, tm.tm_sec);
     std::string str(now);
     text_out = road_name + str;
-    rect_w = 1050;
+    rect_w = 1410; //1050
     rect_x = s->fb_w/2 - rect_w/2;
   } else if (s->scene.top_text_view == 5 && s->scene.osm_enabled) {
     snprintf(now,sizeof(now),"%02d-%02d %s ", tm.tm_mon + 1, tm.tm_mday, dayofweek);
     std::string str(now);
     text_out = road_name + str;
-    rect_w = 850;
+    rect_w = 1050; //850;
     rect_x = s->fb_w/2 - rect_w/2;
   } else if (s->scene.top_text_view == 6 && s->scene.osm_enabled) {
     snprintf(now,sizeof(now),"%02d:%02d:%02d ", tm.tm_hour, tm.tm_min, tm.tm_sec);
     std::string str(now);
     text_out = road_name + str;
-    rect_w = 750;
+    rect_w = 1050; //750;
     rect_x = s->fb_w/2 - rect_w/2;
   } else if (s->scene.top_text_view == 7 && s->scene.osm_enabled) {
     text_out = road_name;
-    rect_w = 500;
+    rect_w = 860; //500
     rect_x = s->fb_w/2 - rect_w/2;
   }
   nvgBeginPath(s->vg);
